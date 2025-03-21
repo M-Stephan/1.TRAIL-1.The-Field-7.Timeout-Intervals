ff42db256e275c5a87f660bb7888a237209122c7
NDC Studio <ndc.dev.code@gmail.com>
2025-03-21 10:20:33 +0100

    Update function setText avec setInterval et clearTimeout

--- js/modules/function.js
+++ js/modules/function.js
@@ -1,9 +1,5 @@
-/** @const {HTMLElement} header -- cible le <header> */
-const header = document.getElementById("header");
-/** @const {HTMLElement} h1 -- cible le <h1> */
-const h1 = document.createElement("h1");
-/** @description ajoute le <h1> dans le <header> */
-header.appendChild(h1)
+/** @var {number} letter -- initialise l'index de la lettre en cours sur 0 */
+let letter = 0;
 
 /**
  * 
@@ -11,11 +7,12 @@ header.appendChild(h1)
  * @description -- Function permettant de taper le mot 1 lettre à la seconde
  * 
  */
-export function setText(word) {
-    for (let i = 0; i < word.length; i++) {
-        setTimeout(function() {
-            h1.innerText += word[i] 
-        }, i * 1000)
+export function setText(word, h1, interval) {
+    if (letter < word.length) {
+        h1.innerText += word[letter];
+        letter++;
+    } else {
+        clearInterval(interval);
     }
 }
 
--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 import { setText } from "./modules/function.js";
 
-const word = "Wozniak"
+/** @const {HTMLElement} header -- cible le <header> */
+const header = document.getElementById("header");
 
-setText(word);
+/** @const {HTMLElement} h1 -- crée un <h1> */
+const h1 = document.createElement("h1");
+
+/** @description ajoute le <h1> dans le <header> */
+header.appendChild(h1);
+
+/** @var {string} word -- mot à afficher */
+const word = "Wozniak";
+
+const interval = setInterval(() => {
+    setText(word, h1, interval);
+}, 1000);
