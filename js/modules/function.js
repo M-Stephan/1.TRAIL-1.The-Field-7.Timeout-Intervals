531cbc4b49a379097d5910d26db869be13422ebd
NDC Studio <ndc.dev.code@gmail.com>
2025-03-21 11:01:32 +0100

    Exercice 1 et 2 completés | + ajout documentation JsDoc

--- js/modules/function.js
+++ js/modules/function.js
@@ -17,4 +17,27 @@ export function setText(word, h1, interval) {
 }
 
 /* --------------------------------------------------------------- */
-
+/** @var {number} sec -- initialise les secondes sur 0 */
+let sec = 0;
+/** @var {number} min -- initialise les minutes sur 0 */
+let min = 0;
+/**
+ * 
+ * @param {string} time
+ * @description -- Function permettant de compter chaques minutes et les afficher sur la page web
+ * 
+ */
+export function setTime(time) {
+    if (sec < 59) {
+        sec++;
+    } else {
+        sec = 0;
+        min++;
+    }
+    console.log(sec)
+    if (min > 0) {
+        time.innerText = `${min} minute has passed`;
+    } else {
+        time.innerText = `${min} minutes have passed`;
+    }
+}
